04d3b02786c9ff2c62962129f093b7cbc324108c
Tạ Quang Khôi <45689286+TaQuangKhoi@users.noreply.github.com>
2022-01-06 17:25:21 +0700

    làm đẹp, format code

--- Insert.sql
+++ Insert.sql
@@ -94,8 +94,8 @@ values ('T254','Sua bo','Long Thanh','1000','800000','VND'),
 ('C2561','Cafe','Trung Nguyen','50000','1200000','VND'),
 ('F524','Duong Phen','Long An','200','410000','VND'),
 ('V0254','Tra','tea plus', '100000','15000000','VND')
- 
 GO 
+
 insert into SanPham (
 MaSanPham,
 TenSanPham,
@@ -109,7 +109,6 @@ values('51','Macha','CongThuc','ThanhPhan','DanhGia','350000đ/ly'),
 ('91','BacSiu','CongThuc','ThanhPhan','DanhGia','30000đ/ly'),
 ('86','KemTraiCay','CongThuc','ThanhPhan','DanhGia','50000đ/ly'),
 ('47','SinhTo','CongThuc','ThanhPhan','DanhGia','40000đ/ly'),
-
 GO
 
 Insert into Menu (
@@ -122,7 +121,6 @@ values('#871f2f','25/05/2016'),
 ('#2c2ff4','28/02/2019'),
 ('#315d27','24/08/2019'),
 ('#e0ccce','28/12/2020'),
-
 GO
 
 Insert into KhoHang (
@@ -131,9 +129,14 @@ Insert into KhoHang (
   DiaChi,
   DienTich,
 )
-values('16862eae87','ChinChin1','F4 Huyen Tran Cong Chua','60m^2'),
-('711c784dd1','ChinChin2','282 Truong Cong Dinh','75m^2'),
-('4edddbaa8a','ChinChin3','49/15 Nguyen Binh Khiem','100m^2'),
-
+values(
+'16862eae87','ChinChin1','F4 Huyen Tran Cong Chua','60m^2'
+),
+(
+'711c784dd1','ChinChin2','282 Truong Cong Dinh','75m^2'
+),
+(
+'4edddbaa8a','ChinChin3','49/15 Nguyen Binh Khiem','100m^2'
+),
 GO
 
