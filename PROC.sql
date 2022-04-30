a0fb61193024434e606d2b5f5c6df76cfe116237
Keios Starqua <khoi0941@gmail.com>
2022-04-30 09:28:47 +0700

    Xóa bỏ cái dấu ngoặc tròn dư thừa

--- PROC.sql
+++ PROC.sql
@@ -1,12 +1,10 @@
 USE ChinhChienVPS
 GO
 create proc CreateAccount
-(
 @tentaikhoan char(10),
 @matkhau varchar(10),
 @loaitaikhoan nvarchar(10),
 @uimode int
-)
 AS
 BEGIN
 INSERT INTO ChinhChienVPS.dbo.TaiKhoan VALUES
