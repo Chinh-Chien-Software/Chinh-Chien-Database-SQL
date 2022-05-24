a0aa7fc2a88539bda951344a8b51fa92af4d9792
Keios Starqua <khoi0941@gmail.com>
2022-05-24 22:14:23 +0700

    Thêm, sửa các constraint của ThanhPhanSanPham
    và Hóa đơn PK

--- Alter.sql
+++ Alter.sql
@@ -16,7 +16,17 @@ FOREIGN key (TenTaiKhoan) REFERENCES TaiKhoan(TenTaiKhoan)
 -- 24 May 2022 21:30 Xóa để tạo mới với tên khác
 alter table HoaDon
 drop CONSTRAINT PK_HoaDon_MaHoaDon
-
 -- 24 May 2022 21:50 
 alter table HoaDon
 add CONSTRAINT PK_MaHoaDon PRIMARY KEY (MaHoaDon)
+
+-- 24-May-2022 22:06
+alter table ThanhPhanSanPham
+add CONSTRAINT PK_ThanhPhanSanPham -- Alter
+    PRIMARY KEY (MaSanPham, MaVatLieu) -- Alter
+
+-- 24-May-2022 22:10 
+alter table ThanhPhanSanPham
+alter COLUMN MaSanPham VARCHAR(3) not null
+alter table ThanhPhanSanPham
+alter COLUMN MaVatLieu VARCHAR(4) not null
